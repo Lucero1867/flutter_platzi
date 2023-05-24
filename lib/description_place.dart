e3d5391256c9b3e14fb48d17d08c7c9ea2c253bb
Lucero Olivos <luceroolivos@gmail.com>
2023-05-24 15:23:19 -0400

    First commit of APP

---
+++ lib/description_place.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class DescriptioPlace extends StatelessWidget {
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  //Constructor
+  DescriptioPlace(this.namePlace, this.descriptionPlace, this.stars,
+      {super.key});
+
+  static String descriptionDummy =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+  @override
+  Widget build(BuildContext context) {
+    final star_half = Container(
+      margin: const EdgeInsets.only(top: 323.0, right: 3.0),
+      child: const Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star_border = Container(
+      margin: const EdgeInsets.only(top: 323.0, right: 3.0),
+      child: const Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star = Container(
+      margin: const EdgeInsets.only(top: 323.0, right: 3.0),
+      child: const Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final title_stars = Row(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.only(top: 320.0, left: 20.0, right: 20.0),
+          child: Text(
+            namePlace,
+            style: const TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            star,
+            star,
+            star,
+            star,
+            star_half,
+          ],
+        )
+      ],
+    );
+
+    final description = Container(
+      margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+      child: Text(
+        descriptionPlace,
+        style: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF56575a)),
+      ),
+    );
+
+    return Column(
+      children: <Widget>[title_stars, description],
+    );
+  }
+}
